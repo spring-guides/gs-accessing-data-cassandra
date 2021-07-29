06bf62cbdf9f296b98085b6ec9de1dc0539f2fd2
Sergi Almar <sergi.almar@gmail.com>
2021-07-29 11:17:10 +0200

    Use CrudRepository instead of CassandraRepository

--- complete/src/main/java/com/example/accessingdatacassandra/VetRepository.java
+++ complete/src/main/java/com/example/accessingdatacassandra/VetRepository.java
@@ -2,8 +2,8 @@ package com.example.accessingdatacassandra;
 
 import java.util.UUID;
 
-import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface VetRepository extends CassandraRepository<Vet, UUID> {	
+public interface VetRepository extends CrudRepository<Vet, UUID> {	
 	Vet findByFirstName(String username);
 }
