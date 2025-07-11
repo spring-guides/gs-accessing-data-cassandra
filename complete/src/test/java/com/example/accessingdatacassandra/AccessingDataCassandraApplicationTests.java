35c424bc7401b5ecfe9e7f8a1f41ed366fb88511
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-07-11 14:12:19 -0600

    Add Test

--- complete/src/test/java/com/example/accessingdatacassandra/AccessingDataCassandraApplicationTests.java
+++ complete/src/test/java/com/example/accessingdatacassandra/AccessingDataCassandraApplicationTests.java
@@ -4,20 +4,33 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.cassandra.CassandraInvalidQueryException;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.SpecificationBuilder;
 import org.springframework.lang.NonNull;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 @SpringBootTest
 class AccessingDataCassandraApplicationTests {
 
+	@Autowired
+	VetRepository vets;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void findByFirstNameWhenNotAllowFilteringThenFails() {
+		assertThatExceptionOfType(CassandraInvalidQueryException.class)
+			.isThrownBy(() -> this.vets.findByFirstName("somename"));
 	}
 
 	@TestConfiguration
