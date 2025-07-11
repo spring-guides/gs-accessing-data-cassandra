c65e929d5b3957ea7ed72df7fec6f01a3d33460f
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-07-11 12:12:44 -0600

    Tests Stand Up Cassandra
    This allows the tests to run and confirm that the guide
    still works.

--- complete/src/main/java/com/example/accessingdatacassandra/AccessingDataCassandraApplication.java
+++ complete/src/main/java/com/example/accessingdatacassandra/AccessingDataCassandraApplication.java
@@ -6,11 +6,11 @@ import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.cassandra.core.CassandraTemplate;
 
 @SpringBootApplication
 public class AccessingDataCassandraApplication {
--- complete/src/test/java/com/example/accessingdatacassandra/AccessingDataCassandraApplicationTests.java
+++ complete/src/test/java/com/example/accessingdatacassandra/AccessingDataCassandraApplicationTests.java
@@ -1,7 +1,17 @@
 package com.example.accessingdatacassandra;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.SpecificationBuilder;
+import org.springframework.lang.NonNull;
 
 @SpringBootTest
 class AccessingDataCassandraApplicationTests {
@@ -10,4 +20,34 @@ class AccessingDataCassandraApplicationTests {
 	void contextLoads() {
 	}
 
+	@TestConfiguration
+	static class CreateKeyspaceConfiguration extends AbstractCassandraConfiguration {
+
+		private final CassandraProperties properties;
+
+		CreateKeyspaceConfiguration(CassandraProperties properties) {
+			this.properties = properties;
+		}
+
+		@NonNull
+		@Override
+		protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+			CreateKeyspaceSpecification specification = SpecificationBuilder
+				.createKeyspace(getKeyspaceName()).ifNotExists()
+				.withSimpleReplication(1);
+
+			return List.of(specification);
+		}
+
+		@Override
+		public SchemaAction getSchemaAction() {
+			return SchemaAction.RECREATE_DROP_UNUSED;
+		}
+
+		@NonNull
+		@Override
+		protected String getKeyspaceName() {
+			return this.properties.getKeyspaceName();
+		}
+	}
 }
